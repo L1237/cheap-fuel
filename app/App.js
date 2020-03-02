c89ea7f1845bce1ff817af4b416213e2688586ad
L1237 <49970990+L1237@users.noreply.github.com>
2020-03-02 05:49:23 +0100

    Update App.js
    price and distance snippet (Levi-Johannes)

--- app/App.js
+++ app/App.js
@@ -37,54 +37,54 @@ const styles = StyleSheet.create({
   },
 });
 
-// function filterStations(station){
-//   for(let i =0; i<Config.types.length;i++){
-//     if(station [config.types[i]] <= 0 || config.showOpenOnly && !station.isOpen){
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-// function normalizeStations(value, index,stations){
-//   stations[index].prices ={
-//     diesel: value.diesel,
-//     petrol: value.petrol,
-//     lpg: value.lpg,
-//   }
-// }
-// stations[index].distance = value.dist;
-//     stations[index].address = `${`0${value.postCode}`.slice(-5)} ${
-//         value.place} - ${value.street} ${value.houseNumber}`;
+ function filterStations(station){
+   for(let i =0; i<Config.types.length;i++){
+     if(station [config.types[i]] <= 0 || config.showOpenOnly && !station.isOpen){
+       return false;
+     }
+   }
+   return true;
+ }
+ function normalizeStations(value, index,stations){
+   stations[index].prices ={
+     diesel: value.diesel,
+     petrol: value.petrol,
+     lpg: value.lpg,
+   }
+ }
+ stations[index].distance = value.dist;
+     stations[index].address = `${`0${value.postCode}`.slice(-5)} ${
+         value.place} - ${value.street} ${value.houseNumber}`;
 
-//         async function getData() {
-//           const response = await fetch(generateUrl());
-//           const parsedResponse = await response.json();
+         async function getData() {
+           const response = await fetch(generateUrl());
+           const parsedResponse = await response.json();
       
       
-//           if (!parsedResponse.ok) {
-//               throw new Error('Error no fuel data');
-//           }
+           if (!parsedResponse.ok) {
+               throw new Error('Error no fuel data');
+           }
       
-//           const stations = parsedResponse.stations.filter(filterStations);
+           const stations = parsedResponse.stations.filter(filterStations);
       
-//           stations.forEach(normalizeStations);
+           stations.forEach(normalizeStations);
       
-//           const price = stations.slice(0);
-//           price.sort(sortByPrice);
+           const price = stations.slice(0);
+           price.sort(sortByPrice);
       
-//           return {
-//               types: ['diesel', 'petrol', 'lpg'],
-//               unit: 'km',
-//               currency: 'ghc',
-//               byPrice: price,
-//               byDistance: stations
-//           };
-//       }
-//       module.exports = options => {
-//         config = options;
+           return {
+               types: ['diesel', 'petrol', 'lpg'],
+               unit: 'km',
+               currency: 'ghc',
+               byPrice: price,
+               byDistance: stations
+           };
+       }
+       module.exports = options => {
+         config = options;
     
-//         return { getData };
-//     };
+         return { getData };
+     };
     
 
 
