146926a793617dae18cc452a1e3c6339725f3238
Levi <levikomi@yahoo.com>
2020-03-02 05:25:35 +0100

    price and disstance snippet

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import{IonIcons,MaterialCommunityIcons}from '@expo/vector-icons';
 
 export default function App() {
   const [gasStops, setGasStops] = useState([]);
@@ -20,6 +21,7 @@ export default function App() {
           <Marker key={gs.name} coordinate={gs.location} title={gs.name} />
         ))}
       </MapView>
+      <MaterialCommunityIcons name='gas-station' size={50} color='red'/>
     </View>
   );
 }
@@ -37,56 +39,56 @@ const styles = StyleSheet.create({
   },
 });
 
-// function filterStations(station){
-//   for(let i =0; i<Config.types.length;i++){
-//     if(station [config.types[i]] <= 0 || config.showOpenOnly && !station.isOpen){
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-// function normalizeStations(value, index,stations){
-//   stations[index].prices ={
-//     diesel: value.diesel,
-//     petrol: value.petrol,
-//     lpg: value.lpg,
-//   }
-// }
-// stations[index].distance = value.dist;
-//     stations[index].address = `${`0${value.postCode}`.slice(-5)} ${
-//         value.place} - ${value.street} ${value.houseNumber}`;
+function filterStations(station){
+   for(let i =0; i<Config.types.length;i++){
+     if(station [config.types[i]] <= 0 || config.showOpenOnly && !station.isOpen){
+      return false;
+    }
+   }   return true;
+ }
+ function normalizeStations(value, index,stations){
+   stations[index].prices ={
+    diesel: value.diesel,
+    petrol: value.petrol,
+    lpg: value.lpg,
+   }
+ }
+ stations[index].distance = value.dist;
+    stations[index].address = `${`0${value.postCode}`.slice(-5)} ${
+        value.place} - ${value.street} ${value.houseNumber}`;
 
-//         async function getData() {
-//           const response = await fetch(generateUrl());
-//           const parsedResponse = await response.json();
+        async function getData() {
+          const response = await fetch(generateUrl());
+          const parsedResponse = await response.json();
       
       
-//           if (!parsedResponse.ok) {
-//               throw new Error('Error no fuel data');
-//           }
+          if (!parsedResponse.ok) {
+              throw new Error('Error no fuel data');
+          }
       
-//           const stations = parsedResponse.stations.filter(filterStations);
+           const stations = parsedResponse.stations.filter(filterStations);
       
-//           stations.forEach(normalizeStations);
+          stations.forEach(normalizeStations);
       
-//           const price = stations.slice(0);
-//           price.sort(sortByPrice);
+          const price = stations.slice(0);
+          price.sort(sortByPrice);
       
-//           return {
-//               types: ['diesel', 'petrol', 'lpg'],
-//               unit: 'km',
-//               currency: 'ghc',
-//               byPrice: price,
-//               byDistance: stations
-//           };
-//       }
-//       module.exports = options => {
-//         config = options;
+          return {
+               types: ['diesel', 'petrol', 'lpg'],
+               unit: 'km',
+              currency: '',
+              byPrice: price,
+              byDistance: stations
+          };
+      }
+       module.exports = options => {
+        config = options;
     
-//         return { getData };
-//     };
+        return { getData };
+     };
     
 
 
 
 
+                          
