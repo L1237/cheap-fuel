9721072c52ce47e3b8e758962ba6c17f9c2ee0b6
L1237 <49970990+L1237@users.noreply.github.com>
2020-04-01 20:48:30 +0200

    Update index.js
    updates

--- backend/resolvers/index.js
+++ backend/resolvers/index.js
@@ -37,3 +37,33 @@ export default {
     }
   },
 };
+async function getPricesFromApi(){
+  try{
+    let response = await fetch('https://www.globalpetrolprices.com/api_gpp.php?cnt=GH&ind=gp,dp,lp,kr&prd=latest&uid=1755&uidc=b75356ac1aad2360c3671a04c0744b46')
+  let responseJson = await response.json();
+  console.log(responseJson);
+}
+catch(error){
+  console.error(error);
+}
+};
+async function getData(){
+  const response = await fetch(generateUrl('https://www.globalpetrolprices.com/api_gpp.php?cnt=GH&ind=gp,dp,lp,kr&prd=latest&uid=1755&uidc=b75356ac1aad2360c3671a04c0744b46'));
+  const parsedResponse = await response.json();
+
+  if(!parsedResponse.ok){
+    throw new Error('Error no fuel data');
+  }
+  const stations = parsedResponse.stations.filter(filterStations);
+  stations.forEach(normalizeStations);
+  const price = stations.slice(0);
+  price.sort(sortByPrice);
+
+  return{
+    types:['diesel','petrol','lpg','kerosene'],
+    unit: 'km',
+    currency: 'ghc',
+    byPrice: price,
+    byDistance: stations
+  }
+}
