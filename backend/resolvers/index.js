637733bf6c6f4c2398b4678e0067d22f1c961a16
L1237 <49970990+L1237@users.noreply.github.com>
2020-04-04 16:58:50 +0200

    Update index.js

--- backend/resolvers/index.js
+++ backend/resolvers/index.js
@@ -48,23 +48,75 @@ catch(error){
   console.error(error);
 }
 };
-async function getData(){
-  const response = await fetch(generateUrl('https://www.globalpetrolprices.com/api_gpp.php?cnt=GH&ind=gp,dp,lp,kr&prd=latest&uid=1755&uidc=b75356ac1aad2360c3671a04c0744b46'));
+function filterStations(station) {
+  for (let i = 0; i < Config.types.length; i++) {
+    if (station[config.types[i]] <= 0 || config.showOpenOnly && !station.isOpen) {
+      return false;
+    }
+  } return true;
+}
+function normalizeStations(value, index, stations) {
+  stations[index].prices = {
+    diesel: value.diesel,
+    gasoline: value.gasoline,
+    lpg: value.lpg,
+    kerosene: value.kerosene,
+  }
+}
+stations[index].distance = value.dist;
+stations[index].address = `${`0${value.postCode}`.slice(-5)} ${
+  value.place} - ${value.street} ${value.houseNumber}`;
+
+async function getData() {
+  const response = await fetch(generateUrl());
   const parsedResponse = await response.json();
 
-  if(!parsedResponse.ok){
+
+  if (!parsedResponse.ok) {
     throw new Error('Error no fuel data');
   }
+
   const stations = parsedResponse.stations.filter(filterStations);
+
   stations.forEach(normalizeStations);
+
   const price = stations.slice(0);
   price.sort(sortByPrice);
 
-  return{
-    types:['diesel','gasoline','lpg','kerosene'],
+  return {
+    types: ['diesel', 'petrol', 'lpg'],
     unit: 'km',
-    currency: 'ghc',
+    currency: 'Ghc',
     byPrice: price,
     byDistance: stations
-  }
+  };
 }
+module.exports = options => {
+  config = options;
+
+  return { getData };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
